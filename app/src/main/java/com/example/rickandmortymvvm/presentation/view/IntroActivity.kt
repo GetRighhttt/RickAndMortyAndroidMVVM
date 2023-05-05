9a6af302d0d011c840618126d0974738a1e24ac5
Stefan Bayne <stefanbusiness95@gmail.com>
2023-05-05 19:16:46 -0400

    finished the splash screen.

---
+++ app/src/main/java/com/example/rickandmortymvvm/presentation/view/IntroActivity.kt
@@ -0,0 +1,50 @@
+package com.example.rickandmortymvvm.presentation.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.rickandmortymvvm.R
+import com.example.rickandmortymvvm.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+    private var _binding: ActivityIntroBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivRm.animate().apply {
+            duration = 200L
+            translationXBy(-360f)
+        }.withEndAction {
+            binding.ivRm.animate().apply {
+                duration = 400L
+                rotationX(360f)
+                translationXBy(360f)
+            }
+        }
+        // delays before moving to other activity
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startActivity(
+                    Intent(this@IntroActivity, RickAndMortyActivity::class.java)
+                )
+                finish()
+            }, 1200
+        )
+    }
+
+    private fun startAnimation() {
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
